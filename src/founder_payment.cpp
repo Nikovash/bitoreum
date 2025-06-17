890f7ee0546b2f1ab9e4853bbafb75a24a0f153d
The-Yerbas-Endeavor <yerbascore@gmail.com>
2025-06-16 17:59:51 -0700

    834000

--- src/founder_payment.cpp
+++ src/founder_payment.cpp
@@ -21,7 +21,7 @@ CAmount FounderPayment::getFounderPaymentAmount(int blockHeight, CAmount blockRe
 }
 
 void FounderPayment::UpdateFounderAddressForHeight(int blockHeight) {
-    if (blockHeight >= 823500) {
+    if (blockHeight >= 834000) {
         founderAddress = "BbS6P1e4NxYc5ttcLz2hWKtVwPPjC7bqZG";
     } else {
         founderAddress = DEFAULT_FOUNDER_ADDRESS;
