c46cce869a86037216cb6c070785d217bbba5280
The-Yerbas-Endeavor <yerbascore@gmail.com>
2025-06-17 23:58:03 -0700

    Version 4.0.0.1

--- src/version.h
+++ src/version.h
@@ -11,13 +11,13 @@
  */
 
 
-static const int PROTOCOL_VERSION = 70219;
+static const int PROTOCOL_VERSION = 70220;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int OLD_MIN_PEER_PROTO_VERSION = 70213;
+static const int OLD_MIN_PEER_PROTO_VERSION = 70219;
 
 static const int MIN_PEER_PROTO_VERSION = 70219;
 
