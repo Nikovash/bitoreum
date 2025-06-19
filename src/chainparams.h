cf998260dd57544e073ca73db86b50e67fe4bfb1
NunyaBidnizz <67256882+Nikovash@users.noreply.github.com>
2025-06-19 00:47:23 -0700

    find out again [skip ci]

--- src/chainparams.h
+++ src/chainparams.h
@@ -106,9 +106,9 @@ public:
         return height >= GetConsensus().nFutureForkBlock;
     };
     bool IsProtocolUpgradeActive(CBlockIndex *index) const {
-+        int height = index == nullptr ? 0 : index->nHeight;
-+        return height >= GetConsensus().nProtocolUpgradeBlock;
-+   };
+         int height = index == nullptr ? 0 : index->nHeight;
+         return height >= GetConsensus().nProtocolUpgradeBlock;
+    };
     const std::vector<std::string>& SporkAddresses() const { return vSporkAddresses; }
     int MinSporkKeys() const { return nMinSporkKeys; }
     bool BIP9CheckSmartnodesUpgraded() const { return fBIP9CheckSmartnodesUpgraded; }
